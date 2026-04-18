48864be3bbae19bba05480a2a268ae05db079fe4
BiiigJo <jonathan.marois@gmail.com>
2026-04-18 22:23:28 +0200

    Add files via upload

--- src/lib/gemini.ts
+++ src/lib/gemini.ts
@@ -4,6 +4,7 @@ import { GoogleGenAI, Type } from "@google/genai";
 const ai = new GoogleGenAI({ 
   apiKey: import.meta.env.VITE_GEMINI_API_KEY! 
 });
+
 export const clothingRecognitionSchema = {
   type: Type.OBJECT,
   properties: {
